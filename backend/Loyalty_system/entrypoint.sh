a8c97811ac2c476eba52ede88f3ba9c5f9d0bf9a
Volodymyr262 <volodiahedz26@gmail.com>
2025-03-19 20:15:33 +0100

    added entrypoint.sh script

---
+++ backend/Loyalty_system/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit immediately if any command fails
+set -e
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+exec "$@"
