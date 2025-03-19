fb0fd6a21ac1f59c8d57d096aa686a3606959190
Volodymyr262 <volodiahedz26@gmail.com>
2025-03-19 20:23:54 +0100

    fixed entrypoint.sh for activation venv

--- backend/Loyalty_system/entrypoint.sh
+++ backend/Loyalty_system/entrypoint.sh
@@ -3,11 +3,18 @@
 # Exit immediately if any command fails
 set -e
 
+# Activate virtual environment
+echo "Activating virtual environment..."
+source /opt/venv/bin/activate
+
+# Run migrations
 echo "Running migrations..."
 python manage.py migrate --noinput
 
+# Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --verbosity 2
 
+# Start Gunicorn
 echo "Starting Gunicorn..."
 exec "$@"
