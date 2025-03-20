84ecccc9c99ae72ab8f34120d0074117cf0751f4
Volodymyr262 <volodiahedz26@gmail.com>
2025-03-20 07:54:11 +0100

    entrypoint.sh fixing

--- backend/Loyalty_system/entrypoint.sh
+++ backend/Loyalty_system/entrypoint.sh
@@ -1,20 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-# Exit immediately if a command exits with a non-zero status
+# Exit immediately if a command fails
 set -e
 
-# Activate virtual environment
 echo "Activating virtual environment..."
-source /opt/venv/bin/activate
+. /opt/venv/bin/activate  # Use "." instead of "source" for POSIX compliance
 
-# Run database migrations
 echo "Running migrations..."
 python manage.py migrate --noinput
 
-# Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --verbosity 2
 
-# Start Gunicorn
 echo "Starting Gunicorn..."
-exec gunicorn Loyalty_system.wsgi:application --bind 0.0.0.0:${PORT}
+exec gunicorn Loyalty_system.wsgi:application --bind 0.0.0.0:${PORT} --workers 3 --log-level info
